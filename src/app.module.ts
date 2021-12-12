dc2a6812bb230ce136c51f203a7e45d93cb152a3
Uporold <vg.ryndin@gmail.com>
2021-12-12 23:10:05 +0300

    add typeorm config and env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import options from './common/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(options)],
 })
 export class AppModule {}
